eddeec8feed32883d3bdcb3fe3e06de518ee1dbb
Michael Coxeter <mcoxeter@gamil.com>
2021-12-22 05:25:47 +0100

    Updated the analyis with more details

--- README.md
+++ README.md
@@ -1,12 +1,88 @@
-# buy-price
+# 04-mos
 
-**Usage**
+This program will attempt to find a buy and sell price for a stock by calculating the intrinsic value of the stock using the discounted cash flow method.
+
+It uses the following site for the caluculation: https://tradebrains.in/dcf-calculator/
+
+The following values are used in the calculation
+
+**Free Cash Flow**
+
+> The average fcf value for the last three years
+
+**Current Long Term Debt**
+
+> The current long term debt can be found on the balance sheet
+
+**Diluted shares outstanding**
+
+> The number of shares in a company if all convertable securities where realized.
+
+**Predicted growth**
+
+> The average growth rate of FCF over the last three years or the analysts prediction.
+
+## Setup
+
+you need to create a config.json file. This will configure the program.
+There is one parameter you need to add.
+
+1. path - This is a folder path to where your output files will be stored on your harddisk.
+
+This is an example of a config.json file:
+
+```json
+{
+  "path": "C:/Business analysis/Evaluation"
+}
+```
+
+## Usage
+
+> Before you run this program, you will need to have run the `01-data` program first on the stock.
+
+In this example the program will score the fundamental data on Facebook
+
 `npm start -- FB`
 
-This program will evaluate the fair-price of a stock using the discounted cash flow method.
-It will output the result into a fix folder:
-`C:\Users\Mike\OneDrive - Digital Sparcs\Investing\Value Investing Process\Business analysis/{stock}`
+## Output
 
-The output file is named fair-value-{execution date}.json
+The output of this program is scoring data in json form. It will be outputted into a sub folder of your path in the config file.
 
-The program relies on core-data being created for the stock before hand. See [core-data](https://github.com/mcoxeter/core-data)
+### Output folder structure
+
+_path_/_stock-name_/05-mos/_date_.json
+
+e.g.
+C:/Business analysis/Evaluation/FB/05-mos/2021.12.18.json
+
+### Example output
+
+```
+{
+  "type": "05-mos",
+  "symbol": "FB",
+  "references": [],
+  "date": "2021.12.22",
+  "calculator": "https://tradebrains.in/dcf-calculator/",
+  "notes": "These values are conservative. You may need to adjust the expectedGrowth based on your deep understanding of the business.",
+  "fcf": "20067666667",
+  "cash_and_equiv": "17576000000",
+  "longTermDebt": "0",
+  "sharesOutstanding": "2888000000",
+  "growthAnalysis": {
+    "FCF": [23632000000, 21212000000, 15359000000],
+    "ourGrowth": 35,
+    "analystsGrowthNext5Years": "21.35",
+    "growth": "21.35",
+    "growthNotes": "We chose the analysts growth calculation. ourGrowth is too high."
+  },
+  "expectedGrowth": "21",
+  "discountRate": "15",
+  "multiple": "10",
+  "mos": "50",
+  "currentPrice": "333.79",
+  "sellPrice": 214.42,
+  "buyPrice": 107.21
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,15 @@
 #!/usr/bin/env node
 import { Page, webkit } from 'playwright';
 import fs from 'fs';
+const config = require('./config.json');
 
 async function app() {
   var myArgs = process.argv.slice(2);
   const symbol = myArgs[0];
 
-  const path = `C:/Users/Mike/OneDrive - Digital Sparcs/Investing/Value Investing Process/Business analysis/Evaluation/${symbol}/`;
+  const path = `${config.path}/${symbol}`;
 
-  const lastDataFile = fs
-    .readdirSync(`${path}/core`)
-    .filter((file) => file.endsWith('.json'))
-    .sort((a, b) => b.localeCompare(a))
-    .find(() => true);
-
-  const stats = require(`${path}/core/${lastDataFile}`);
-
-  const browser = await webkit.launch({
-    headless: true
-  });
-  const page = await browser.newPage();
-
-  const sharesOutstanding =
-    stats['Implied Shares Outstanding 6'] !== 'N/A'
-      ? stats['Implied Shares Outstanding 6']
-      : stats['Shares Outstanding 5'];
-
-  let growth = 15;
-
-  const ourGrowth = Number(stats['Growth'].replace('%', ''));
-  const analystGrowth = Number(
-    stats['Growth Next 5 Years (per annum)'].replace('%', '')
-  );
-
-  if (ourGrowth > 20) {
-    growth = analystGrowth;
-  } else {
-    growth = ourGrowth;
-  }
-
-  const input = {
-    calculator: 'https://tradebrains.in/dcf-calculator/',
-    symbol,
-    fcf: Math.round(stats['FreeCashFlowAverage']).toString(),
-    cash: Math.round(stats['Total Cash (mrq)']).toString(),
-    debt: Math.round(stats['Total Debt (mrq)']).toString(),
-    sharesOutstanding: Math.round(sharesOutstanding).toString(),
-    expectedGrowth: Math.round(growth).toString(),
-    discountRate: '15',
-    multiple: '10',
-    mos: '50',
-    price: stats['Price'].toString(),
-    intrinsicValue: ''
-  };
-
-  await page.goto(input.calculator);
-  await enterString(page, '#fieldname4_1', input.fcf);
-  await enterString(page, '#fieldname2_1', input.cash);
-  await enterString(page, '#fieldname3_1', input.debt);
-
-  await enterString(page, '#fieldname5_1', input.sharesOutstanding);
-
-  await enterString(page, '#fieldname6_1', input.expectedGrowth);
-  await enterString(page, '#fieldname7_1', input.discountRate);
-  await enterString(page, '#fieldname8_1', input.multiple);
-  await enterString(page, '#fieldname77_1', input.mos);
-  await enterString(page, '#fieldname80_1', input.price);
-
-  const intrinsicValue = await page.waitForSelector('#fieldname74_1');
-
-  input.intrinsicValue = (await intrinsicValue.inputValue()).toString();
+  const requiredPaths = [path, `${path}/05-mos`];
 
   const nowDate = new Date();
   const padNum = (num: number) => num.toString().padStart(2, '0');
@@ -78,9 +18,109 @@ async function app() {
     nowDate.getMonth() + 1
   )}.${padNum(nowDate.getDate())}`;
 
+  requiredPaths.forEach((p) => {
+    if (!fs.existsSync(p)) {
+      fs.mkdirSync(p);
+    }
+  });
+
+  const lastDataFile = fs
+    .readdirSync(`${path}/01-data`)
+    .filter((file) => file.endsWith('.json'))
+    .sort((a, b) => b.localeCompare(a))
+    .find(() => true);
+
+  const stats = require(`${path}/01-data/${lastDataFile}`);
+
+  const browser = await webkit.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+
+  const ourGrowth = Number(stats['Growth'].replace('%', ''));
+  const annual = stats.data.data.financials.annual;
+
+  const analystsGrowthNext5Years = stats[
+    'Growth Next 5 Years (per annum)'
+  ].replace('%', '');
+  let growthNotes = 'We chose our growth calculation. calcuated from the FCF.';
+  let growth = ourGrowth;
+  if (ourGrowth > 20) {
+    growthNotes =
+      'We chose the analysts growth calculation. ourGrowth is too high.';
+    growth = analystsGrowthNext5Years;
+  }
+
+  if (growth > 25) {
+    growthNotes += ` We capped the growth to 25, as ${growth} is too high for us.`;
+    growth = 25;
+  }
+
+  const growthAnalysis = {
+    FCF: stats['FFC'],
+    ourGrowth: ourGrowth,
+    analystsGrowthNext5Years,
+    growth,
+    growthNotes
+  };
+
+  const lt_debt10 = lastNFromArray<number>(10, annual.lt_debt);
+  const currentLongTermDebt = lt_debt10[9];
+
+  const shares_diluted10 = lastNFromArray<number>(10, annual.shares_diluted);
+  const currentShares_diluted = shares_diluted10[9];
+
+  const cash_and_equiv10 = lastNFromArray<number>(10, annual.cash_and_equiv);
+  const currentcash_and_equiv = cash_and_equiv10[9];
+
+  const input = {
+    type: '05-mos',
+    symbol,
+    references: [],
+    date: nowDateStr,
+    calculator: 'https://tradebrains.in/dcf-calculator/',
+    notes:
+      'These values are conservative. You may need to adjust the expectedGrowth based on your deep understanding of the business.',
+    fcf: Math.round(stats['FreeCashFlowAverage']).toString(),
+    cash_and_equiv: Math.round(currentcash_and_equiv).toString(),
+    longTermDebt: Math.round(currentLongTermDebt).toString(),
+    sharesOutstanding: Math.round(currentShares_diluted).toString(),
+    growthAnalysis,
+    expectedGrowth: Math.round(growth).toString(),
+    discountRate: '15',
+    multiple: '10',
+    mos: '50',
+    currentPrice: stats['Price'].toString(),
+    sellPrice: 0,
+    buyPrice: 0
+  };
+
+  await page.goto(input.calculator);
+  await enterString(page, '#fieldname4_1', input.fcf);
+  await enterString(page, '#fieldname2_1', input.cash_and_equiv);
+  await enterString(page, '#fieldname3_1', input.longTermDebt);
+
+  await enterString(page, '#fieldname5_1', input.sharesOutstanding);
+
+  await enterString(page, '#fieldname6_1', input.expectedGrowth);
+  await enterString(page, '#fieldname7_1', input.discountRate);
+  await enterString(page, '#fieldname8_1', input.multiple);
+  await enterString(page, '#fieldname77_1', input.mos);
+  await enterString(page, '#fieldname80_1', input.currentPrice);
+
+  const intrinsicValueAfterDiscount = await page.waitForSelector(
+    '#fieldname74_1'
+  );
+  input.sellPrice =
+    Number(await intrinsicValueAfterDiscount.inputValue()) *
+    (100 / Number(input.mos));
+
+  input.buyPrice = Number(await intrinsicValueAfterDiscount.inputValue());
+
+  console.log('Writing ', `${path}/05-mos/${nowDateStr}.json`);
   try {
     fs.writeFileSync(
-      `${path}/fair-value-${nowDateStr}.json`,
+      `${path}/05-mos/${nowDateStr}.json`,
       JSON.stringify(input, undefined, 4)
     );
   } catch (err) {
@@ -99,3 +139,7 @@ async function enterString(page: Page, id: string, value: string) {
 }
 
 app();
+
+function lastNFromArray<T>(n: number, values: T[]): T[] {
+  return values.slice(-n);
+}
